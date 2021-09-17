cbc35e3b265b6bfac678faf3ce7bec584175c06e
ba144220 <hs41511@gmail.com>
2021-09-17 21:08:39 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>哈囉，我們是台大合唱團www</h1>
+    </div>
+  );
+}
+
+export default App;
