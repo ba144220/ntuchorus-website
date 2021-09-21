3d1786c00727da72751057541b20eaa47171444c
ba144220 <hs41511@gmail.com>
2021-09-22 03:20:45 +0800

    merge

--- src/Containers/Footer/Footer.js
+++ src/Containers/Footer/Footer.js
@@ -17,21 +17,11 @@ const useStyle = makeStyles((theme) => ({
     },
 }));
 
->>>>>>> yuchi_dev
 const Footer = () => {
     return (
         <div className="FootPage">
             <div className="TagContainer">
                 <a href="https://www.facebook.com/NTUChorus/">
-<<<<<<< HEAD
-                    <i className="fa fa-facebook-square" style={{ fontSize: "24px" }}></i>
-                </a>
-                <a href="https://www.instagram.com/ntuchorus/">
-                    <i className="fa fa-instagram" style={{ fontSize: "24px" }}></i>
-                </a>
-                <a href="https://www.youtube.com/user/ntuchorus">
-                    <i className="fa fa-youtube-play" style={{ fontSize: "24px" }}></i>
-=======
                     <Facebook fontSize="large" className={classes.icons} />
                 </a>
                 <a href="https://www.instagram.com/ntuchorus/">
@@ -39,7 +29,6 @@ const Footer = () => {
                 </a>
                 <a href="https://www.youtube.com/user/ntuchorus">
                     <Youtube fontSize="large" className={classes.icons} />
->>>>>>> yuchi_dev
                 </a>
             </div>
             <p>Copyright © 2021 NTUChorus</p>
